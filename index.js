f5c14254cbe72099bac7049a152dd8a5d629a805
Eric Rogers <eric.rog17@gmail.com>
2017-11-06 04:30:10 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -13,3 +13,16 @@ function logShout(string) {
 function logWhisper(string) {
   console.log(string.toLowerCase());
 }
+
+function sayHiToGrandma(string) {
+  if (string === string.toUpperCase) {
+    var message = "I can't hear you!";
+    return message;
+  } else if (string === string.toUpperCase) {
+    message = "YES INDEED!";
+    return message;
+  } else if (string === "I love you, Grandma.") {
+    message = "I love you, too.";
+    return message;
+  }
+}
